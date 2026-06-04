1d61638d61d189f2d5dfc0afd538a57d42bbc207
David Gili <d.gili.lopez@gmail.com>
2026-06-04 13:04:48 -0400

    prueba codigo 200 funciona

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -2,4 +2,10 @@
 const request = require("supertest");
 const server = require("../index");
 
-describe("Operaciones CRUD de cafes", () => {});
+describe("Operaciones CRUD de cafes", () => {
+  it("Obteniendo código 200", async () => {
+    const response = await request(server).get("/cafes"); //Capturamos
+    const status = response.statusCode;
+    expect(status).toBe(200);
+  });
+});

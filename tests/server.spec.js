0e09c046e40a488b3788424e65165d60f1ee4a08
David Gili <d.gili.lopez@gmail.com>
2026-06-05 10:37:30 -0400

    tests ruta get funcionan

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,11 +1,13 @@
-/// <reference types="jest" />
+// <reference types="jest" />
 const request = require("supertest");
 const server = require("../index");
 
 describe("Operaciones CRUD de cafes", () => {
-  it("Obteniendo código 200", async () => {
-    const response = await request(server).get("/cafes"); //Capturamos
-    const status = response.statusCode;
-    expect(status).toBe(200);
+  //Bloque de tests
+  it("Tests ruta GET /cafes", async () => {
+    const { statusCode, body: cafes } = await request(server).get("/cafes"); //Extraemos el statuscode y el objeto con los datos de cafes de la respuesta de la API
+    expect(statusCode).toBe(200); //Comprobamos si el statuscode que devuelve es el 200
+    expect(cafes).toBeInstanceOf(Object); //Comprobamos si los datos de cafes es un objeto
+    expect(cafes.length).toBeGreaterThanOrEqual(1); //Comprobamos si el arreglo contiene 1 o más objetos
   });
 });
